024c9624fb11c406f6da13b1fe3c00cb2bb8b7ab
Brian Cilenti <briancilenti@gmail.com>
2024-11-28 12:49:34 -0500

    chore: boiler

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,17 +1,14 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from './+types/home'
+import { Welcome } from '../welcome/welcome'
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+  return [{ title: 'New React Router App' }, { name: 'description', content: 'Welcome to React Router!' }]
 }
 
 export function loader({ context }: Route.LoaderArgs) {
-  return { message: context.VALUE_FROM_VERCEL };
+  return { message: context.VALUE_FROM_VERCEL }
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
-  return <Welcome message={loaderData.message} />;
+  return <Welcome message={loaderData.message} />
 }
